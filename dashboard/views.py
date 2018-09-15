2008ee543554e2491346af2129219e675c3059f7
Jesco Freund <jesco.freund@my-universe.com>
2018-09-15 07:46:50 +0200

    small improvement to settings dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from settings.api import create_or_update_setting
 from . import forms
 
@@ -18,6 +19,7 @@ def settings(request):
             key = form.cleaned_data['key']
             value = eval(form.cleaned_data['value'])
             create_or_update_setting(key, value)
+            messages.success(request, '<strong>{}</strong> successfully updated.'.format(form.cleaned_data['key']))
             return HttpResponseRedirect(reverse('dashboard:settings'))
     else:
         form = forms.SettingForm()
